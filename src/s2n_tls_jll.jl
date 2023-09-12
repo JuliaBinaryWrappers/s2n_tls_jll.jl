22c8f8337c8b84df57396a9ee782c915eff03269
jlbuild <juliabuildbot@gmail.com>
2023-09-12 23:49:16 +0000

    s2n_tls_jll build 1.3.51+0

---
+++ src/s2n_tls_jll.jl
@@ -0,0 +1,9 @@
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule s2n_tls_jll
+using Base
+using Base: UUID
+import JLLWrappers
+
+JLLWrappers.@generate_main_file_header("s2n_tls")
+JLLWrappers.@generate_main_file("s2n_tls", UUID("cddc5d3d-934d-5d3a-9747-62fc12ea3f48"))
+end  # module s2n_tls_jll
