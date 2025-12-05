4b0e91342ac32e01a4fb001002f4df21feb92870
jlbuild <juliabuildbot@gmail.com>
2025-12-05 07:41:02 +0000

    s2n_tls_jll build 1.6.2+0

--- src/s2n_tls_jll.jl
+++ src/s2n_tls_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("s2n_tls")
-JLLWrappers.@generate_main_file("s2n_tls", UUID("cddc5d3d-934d-5d3a-9747-62fc12ea3f48"))
+JLLWrappers.@generate_main_file("s2n_tls", Base.UUID("cddc5d3d-934d-5d3a-9747-62fc12ea3f48"))
 end  # module s2n_tls_jll
